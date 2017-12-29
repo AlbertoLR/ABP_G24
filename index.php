8338dccc36e63381ef777bac60e2ed7a6f1d4848
AlbertoLR <alber.lpz@gmail.com>
2017-12-30 00:54:41 +0100

    Layout e index arreglado

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <?php
 session_start();
 include "core/Login.php";
+include "layout/menus.php";
+include "layout/footer.php";
 estaRegistrado();
 ?>
 
@@ -38,185 +40,9 @@ estaRegistrado();
  <body>
 
     <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Gimnasio ABP</a>
-            </div>
-            <!-- Top Menu Items -->
-            <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
-                    <ul class="dropdown-menu message-dropdown">
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="message-footer">
-                            <a href="#">Read All New Messages</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> <b class="caret"></b></a>
-                    <ul class="dropdown-menu alert-dropdown">
-                        <li>
-                            <a href="#">Alert Name <span class="label label-default">Alert Badge</span></a>
-                        </li>
-                        <li>
-                            <a href="#">Alert Name <span class="label label-primary">Alert Badge</span></a>
-                        </li>
-                        <li>
-                            <a href="#">Alert Name <span class="label label-success">Alert Badge</span></a>
-                        </li>
-                        <li>
-                            <a href="#">Alert Name <span class="label label-info">Alert Badge</span></a>
-                        </li>
-                        <li>
-                            <a href="#">Alert Name <span class="label label-warning">Alert Badge</span></a>
-                        </li>
-                        <li>
-                            <a href="#">Alert Name <span class="label label-danger">Alert Badge</span></a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">View All</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['Nombre'] ?> <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="core/Desconexion.php"><i class="fa fa-fw fa-power-off"></i> Cerrar Sesión</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-            <!-- Menu lateral -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="index.php"><i class="inicio"></i>Inicio</a>
-                    </li>
 <?php
-    if($_SESSION['Id_PerfilUsuario']==1){
-			echo	'<li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#usuario"><i class="user"></i> Usuarios <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="usuario" class="collapse">
-                            <li>
-                                <a href="controller/CUsuario.php?action=alta" >Nuevo Usuario</a>
-                            </li>
-                            <li>
-                                <a href="controller/CUsuario.php?action=modificacion">Modificar Usuario</a>
-                            </li>
-							<li>
-                                <a href="controller/CUsuario.php?action=baja">Borrar Usuario</a>
-                            </li>
-							<li>
-                                <a href="controller/CUsuario.php?action=consulta">Buscar Usuario</a>
-                            </li>
-                        </ul>
-                    </li>
-					<li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#actividad"><i class="activity"></i> Actividades <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="actividad" class="collapse">
-                            <li>
-                                <a href="controller/CActividad2.php?action=alta" >Nueva Actividad</a>
-                            </li>
-                            <li>
-                                <a href="controller/CActividad2.php?action=modificacion">Modificar Actividad</a>
-                            </li>
-							<li>
-                                <a href="controller/CActividad2.php?action=baja">Borrar Actividad</a>
-                            </li>
-							<li>
-                                <a href="controller/CActividad2.php?action=consulta">Buscar Actividad</a>
-                            </li>
-							</ul>
-                    </li>
-					<li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#recurso"><i class="resource"></i> Recursos <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="recurso" class="collapse">
-                            <li>
-                                <a href="controller/CRecurso.php?action=alta" >Nuevo Recurso</a>
-                            </li>
-                            <li>
-                                <a href="controller/CRecurso.php?action=modificacion">Modificar Recurso</a>
-                            </li>
-							<li>
-                                <a href="controller/CRecurso.php?action=baja">Borrar Recurso</a>
-                            </li>
-							<li>
-                                <a href="controller/CRecurso.php?action=consulta">Buscar Recurso</a>
-                            </li>
-                        </ul>
-                    </li>';
-	}?>					
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
-
+       menus();
+?>
         <div id="page-wrapper">
 		
             <div class="container-fluid">
@@ -473,33 +299,9 @@ estaRegistrado();
 
     </div>
     <!-- /#wrapper -->
-
-			<footer>
-				<div class="row">
-					<div class="col-lg-6 col-md-6">
-						<ul>
-							<li>
-								<a href="#">¿Quiénes somos?</a>
-							</li>
-							<li>
-								<a href="#">Contacta con nosotros</a>
-							</li>
-							<li>
-								<a href="#">Horario de apertura</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-lg-1 col-md-6">
-						<a href="#"><img class="imagenes_fondo" src="images/fb.png" height="64" width="64"></a>
-					</div>
-					<div class="col-lg-1 col-md-6">
-						<a href="#"><img class="imagenes_fondo" src="images/twitter.png" height="64" width="64"></a>
-					</div>
-					<div class="col-lg-1 col-md-6">
-						<a href="#"><img class="imagenes_fondo" src="images/instagram.png" height="64" width="64"></a>
-					</div>
-				</div>
-			</footer>	
+<?php
+	footer();
+?>
 	
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
---
+++ layout/footer.php
@@ -0,0 +1,31 @@
+<?php
+
+function footer(){
+	echo '<footer>
+				<div class="row">
+					<div class="col-lg-6 col-md-6">
+						<ul>
+							<li>
+								<a href="#">¿Quiénes somos?</a>
+							</li>
+							<li>
+								<a href="#">Contacta con nosotros</a>
+							</li>
+							<li>
+								<a href="#">Horario de apertura</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-1 col-md-6">
+						<a href="#"><img class="imagenes_fondo" src="images/fb.png" height="64" width="64"></a>
+					</div>
+					<div class="col-lg-1 col-md-6">
+						<a href="#"><img class="imagenes_fondo" src="images/twitter.png" height="64" width="64"></a>
+					</div>
+					<div class="col-lg-1 col-md-6">
+						<a href="#"><img class="imagenes_fondo" src="images/instagram.png" height="64" width="64"></a>
+					</div>
+				</div>
+			</footer>';
+	
+}
