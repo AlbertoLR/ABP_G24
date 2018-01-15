ff667db8c93d343ec9eeb48d397a8acdac6b1a1d
Iago Nóvoa González <ianovoagonzalez@gmail.com>
2018-01-15 19:13:24 +0100

    Vista de login

--- view/VLogin.php
+++ view/VLogin.php
@@ -5,35 +5,56 @@
  * @author iago
  */
 
+include_once "../layout/menus.php";
+include_once "../layout/footer.php";
+
 class VLogin {
     function __construct(){
         $this->render();
     }
     
     function render(){
+        menus();
 ?>
-       <html>
-            <head>
-                <title>Login</title>
-            </head>
-            <body>
-                <h2>Formulario de login:</h2>
+        <div id="page-wrapper">
+		
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Login <small>Iniciar sesion</small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+                
                 <form action="../controller/CLogin.php" method="post">
-                    <div>
+                    <div class="apartado">
                         <label for="dni">Usuario:</label>
                         <input type="text" name="dni" size="9"/>
                     </div>
-                    <div>
+                    <div class="apartado">
                         <label for="password">Password:</label>
                         <input type="password" name="password" size="50"/>
                     </div>
-                    <div>
+                    <div class="apartado">
                         <button type="submit" name="action" value="comprobar">Enviar</button>
                         <button type="reset" name="reset" value="Borrar">Borrar</button>
                     </div>
                 </form>
-            </body>
-	</html>
+                
+            </div>
+            <!-- /.container-fluid -->
+		
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
 <?php
+	footer();
     }
 }
+
