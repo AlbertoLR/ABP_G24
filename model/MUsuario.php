12bd783c2645d7da2b6b662278a099046139da36
Samuel Gonzalez Veloso <samuelgonzalez.93@gmail.com>
2018-01-16 14:19:08 +0100

    Add files via upload

--- model/MUsuario.php
+++ model/MUsuario.php
@@ -9,16 +9,21 @@
 class MUsuario {
     var $Id_usuario;
     var $Nombre;
+    var $Apellido;
     var $DNI;
+    var $Telefono;
     var $Id_PerfilUsuario;
+    var $password;
     var $mysqli; //atributo manUsador de la BD
     
-    function __construct($Id_usuario,$Nombre,$DNI,$Id_PerfilUsuario){
+    function __construct($Id_usuario,$Nombre,$Apellido,$DNI,$Telefono,$Id_PerfilUsuario,$password){
         $this->Id_usuario=$Id_usuario;
         $this->Nombre=$Nombre;
+        $this->Apellido=$Apellido;
         $this->DNI=$DNI;
+        $this->Telefono=$Telefono;
         $this->Id_PerfilUsuario=$Id_PerfilUsuario;
-        
+         $this->password=$password;
         //incluimos de manera unitaria la funcion de conexion a la BD
         include_once "../core/ConexionBD.php";
         $this->mysqli=conexionBD();
@@ -30,7 +35,7 @@ class MUsuario {
             $sql="SELECT * FROM Usuario WHERE Nombre='$this->Nombre'";
             $resultado= $this->mysqli->query($sql);
             if ($resultado->num_rows==0) { //comprobamos q no exita ya un Us con ese nombre
-                $sql = "INSERT INTO Usuario (Nombre,DNI,Id_PerfilUsuario) VALUES ('$this->Nombre','$this->DNI','$this->Id_PerfilUsuario')";
+                $sql = "INSERT INTO Usuario (Nombre,Apellido,DNI,Telefono,Id_PerfilUsuario,password) VALUES ('$this->Nombre','$this->Apellido','$this->DNI','$this->Telefono','$this->Id_PerfilUsuario','$this->password')";
                 $this->mysqli->query($sql);
                 return "Inserción realizada con éxito";
             }
@@ -48,7 +53,7 @@ class MUsuario {
         $sql="SELECT * FROM Usuario WHERE Id_usuario='$this->Id_usuario'";
         $resultado= $this->mysqli->query($sql);
         if ($resultado->num_rows==1) { //si encuentra la tupla a borrar
-            $sql = "DELETE FROM Usuario WHERE (Id_usuario='$this->Id_usuario')";
+            $sql = "DELETE FROM Usuario WHERE Id_usuario='$this->Id_usuario'";
             $this->mysqli->query($sql);
             return "Borrado correctamente";
         }
@@ -68,19 +73,41 @@ class MUsuario {
             }
             else{
                 $Nombre= $this->Nombre;
+
+
+                }if($this->Apellido==""){ //si descripcion esta vacia se le añade el q ya tiene
+                $Apellido=$tupla[2];
+            }
+            else{
+                $Apellido= $this->Apellido;
+
+
+
+
             }if($this->DNI==""){ //si descripcion esta vacia se le añade el q ya tiene
-                $DNI=$tupla[2];
+                $DNI=$tupla[3];
             }
             else{
                 $DNI= $this->DNI;
-            
+
+
+
+            }if($this->Telefono==""){ //si descripcion esta vacia se le añade el q ya tiene
+                $Telefono=$tupla[4];
+            }
+            else{
+                $Telefono= $this->Telefono;
+
+
+
+
             }if($this->Id_PerfilUsuario==""){ //si descripcion esta vacia se le añade el q ya tiene
-                $Id_PerfilUsuario=$tupla[3];
+                $Id_PerfilUsuario=$tupla[5];
             }
             else{
                 $Id_PerfilUsuario= $this->Id_PerfilUsuario;
             }
-            $sql = "UPDATE Usuario SET Nombre='$Nombre',DNI='$DNI',Id_PerfilUsuario='$Id_PerfilUsuario' WHERE Id_usuario=$this->Id_usuario";
+            $sql = "UPDATE Usuario SET Nombre='$Nombre',Apellido='$Apellido',DNI='$DNI',Telefono='$Telefono',Id_PerfilUsuario='$Id_PerfilUsuario' WHERE Id_usuario=$this->Id_usuario";
             $this->mysqli->query($sql);
             return "Modificado correctamente";
         }
